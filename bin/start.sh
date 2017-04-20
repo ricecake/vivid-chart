fcf1ef1a9c4a3c5eec62e8b82eae4181c00a10b8
Sebastian Green-Husted <ricecake@tfm.nu>
2017-04-19 23:45:43 -0400

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,1 +1,1 @@
-screen -d -m -S vchart_app /bin/sh -c "/home/vchart/vchart/bin/python /home/vchart/vivid-chart/bin/server.py"
+screen -d -m -S vchart_app /bin/sh -c "python /home/vchart/vivid-chart/bin/server.py"
